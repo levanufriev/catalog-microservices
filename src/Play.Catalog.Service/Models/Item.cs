701942bbfbd777b428493168e1459d463b7f25c3
levanufriev02 <levanufriev02@gmail.com>
2022-10-19 13:19:16 +0300

    Add model. Add controller. Add repository.

---
+++ src/Play.Catalog.Service/Models/Item.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Play.Catalog.Service.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+    }
+}
